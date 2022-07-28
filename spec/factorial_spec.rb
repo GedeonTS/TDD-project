7a40a0154baf76c68e4c284cd581c9f0ecd7961b
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-28 13:32:40 +0200

    Create test for fizzbuzz

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -23,6 +23,27 @@ describe ".Solver" do
             expect(solver.reverse("hello")).to eq("olleh")
             end
         end
+        describe "fizzbuzz" do
+            it "should return fizz for multiples of 3" do
+                solver = double
+                allow(solver).to receive(:fizzbuzz).with(3).and_return("fizz")
+                expect(solver.fizzbuzz(3)).to eq("fizz")
+                end
+            it "should return buzz for multiples of 5" do
+                solver = double
+                allow(solver).to receive(:fizzbuzz).with(5).and_return("buzz")
+                expect(solver.fizzbuzz(5)).to eq("buzz")
+                end
+            it "should return fizzbuzz for multiples of 3 and 5" do
+                solver = double
+                allow(solver).to receive(:fizzbuzz).with(15).and_return("fizzbuzz")
+                expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
+                end
+            it "should return the number for non-multiples of 3 or 5" do
+                solver = double
+                allow(solver).to receive(:fizzbuzz).with(7).and_return(7)
+                expect(solver.fizzbuzz(7)).to eq(7)
+                end
    
         end
 end
